8c38d90bdaabda7411eb80831fbf6798c106f7d3
Joey Conroy <come2thecabaret@gmail.com>
2017-02-06 18:43:11 -0500

    grid and reset working without resizing

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-  var square = $("<div class='square'>i</div>");
-  for (i = 0; i < 16; i++) {
-    $("<div class='row' id='" + i + "'></div>").appendTo('#gridContainer');
-    for (j = 0; j < 16; j++) {
-      $("<div class='square'>").appendTo('#gridContainer > #' + i);
+  var dim = 20;
+  var drawGrid = function(dim) {
+    for (i = 0; i < dim; i++) {
+      $("<div class='squareRow' id='" + i + "'></div>").appendTo('#gridContainer');
+      for (j = 0; j < dim; j++) {
+        var square = $("<div class='square'></div>");
+        $(square).appendTo('#gridContainer > #' + i);
+      }
     }
+  };
 
-  }
+  $("#clear button").click(function(){
+    $("#gridContainer .square").removeClass('toggled');
+    var dimensions = prompt("How many squares horizontally and vertically should the new grid contain?");
+    drawGrid(dimensions);
+  });
+  drawGrid(dim);
+  $(".square").hover(function(){
+    console.log("hover");
+    $(this).addClass("toggled");
+  });
 });
